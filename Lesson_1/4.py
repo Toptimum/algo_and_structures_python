f9dd77f4c0b5aeb55f906e50a2227ec2e74f49a0
Taras <info@toptimum.ru>
2019-11-16 13:05:22 +0300

    Дмитрий, здравствуйте.
    Сделал все задачи, кроме двух. 4-я не работает, а 9-я не всегда верно срабатывает. В общем их нужно еще доработать. Остальные задачи решены.

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,24 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+
+
+# не решена
+
+def func_input():
+    print("Сгенерируем случайное целое число, вещественное число или символ в диапазоне.")
+    begin_range = input("Введите начало диапазона: ")
+    end_range = input("и конец диапазона: ")
+    if begin_range > end_range:  # начало диапазона должно быть меньше конца
+        begin_range, end_range = end_range, begin_range
+    return begin_range, end_range
+
+
+def func_generate(begin_range, end_range):
+    rand_num = int(begin_range) + int(end_range)
+    return rand_num
+
+
+begin_range, end_range = func_input()
+
+print(f"Случайное значение в диапазоне ({begin_range};{end_range}): {func_generate(begin_range, end_range)}.")
