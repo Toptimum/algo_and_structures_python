86f491f6801073ed3515f1b4ef5b23d826cd58ed
Taras <info@toptimum.ru>
2019-12-11 13:07:01 +0300

    Мне показалось не эффективным изменять переменные в вашем коде, поэтому я нашел в интернете альтернативные решения задач. Постарался в них разобраться и их доработать или чуть изменить. В 1-й задаче прокомментирована каждая строка, это удобно и можно разобраться в коде. Во 2-й задаче я упростил решение и применил хэш-функцию md5, потому что она одна из популярных. В итоге в коде только одна "сложная" строчка.

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,19 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+
+# вдохновение https://pastebin.com/zaEiu1t2
+
+from hashlib import md5
+
+STRING = input("Введите строку: ")
+
+substrings = {}
+
+print("Различные подстроки в строке и их хэши: ")
+for i in range(len(STRING)):
+    for j in range(len(STRING) - 1 if i == 0 else len(STRING), i, -1):
+        hash = md5(STRING[i:j].encode('utf-8')).hexdigest()
+        substrings[STRING[i:j]] = hash
+        print(f"'{STRING[i:j]}' - {hash}")
+print(f"В исходной строке найдено {len(substrings)} различных подстрок.")
